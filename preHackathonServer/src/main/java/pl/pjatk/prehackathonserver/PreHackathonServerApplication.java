148b74591da85d2066b0deb9e16ffb1d2afc0d5e
JakubKonkol <s24406@pjwstk.edu.pl>
2023-05-28 18:54:24 +0200

    initial commit

---
+++ preHackathonServer/src/main/java/pl/pjatk/prehackathonserver/PreHackathonServerApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.prehackathonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreHackathonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreHackathonServerApplication.class, args);
+	}
+
+}
---
+++ preHackathonServer/src/test/java/pl/pjatk/prehackathonserver/PreHackathonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.prehackathonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreHackathonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
